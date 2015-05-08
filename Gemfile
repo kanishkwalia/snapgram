41ea32afca54e52f5e4c0a10a04ebab02e519a30
Kanishk Walia <kanishk.walia@gmail.com>
2015-05-08 15:40:42 +0100

    Added review functionality

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ group :development, :test do
   gem 'rspec-rails'
 
   gem 'capybara'
-  
+
+  gem 'shoulda'
+
 end
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @post.comments.create(comment_params)
+    redirect_to posts_path
   end
 
   def comment_params
